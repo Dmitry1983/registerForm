ab43e6acacd3704edc85e01acf8c37914b4d9399
Dmitry1983 <barbash.dv@gmail.com>
2022-05-15 11:28:07 +0300

    Update validateAuth.js

--- src/validateAuth.js
+++ src/validateAuth.js
@@ -17,19 +17,32 @@ export default function validateAuth(values) {
   if (!values.phone) {
     errors.phone = 'Required Phone';
   } else if (!regPhone.test(String(values.phone))) {
-    errors.phone = 'Invalid phone number';
+    errors.phone = 'Invalid phone number, format: +70001234567';
   }
   // Email Errors
+  const regEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   if (!values.email) {
     errors.email = 'Required Email';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+  } else if (!regEmail.test(values.email)) {
     errors.email = 'Invalid email address';
   }
   // Password Errors
+  //   ^                 # start-of-string
+  // (?=.*[0-9])       # a digit must occur at least once
+  // (?=.*[a-z])       # a lower case letter must occur at least once
+  // (?=.*[A-Z])       # an upper case letter must occur at least once
+  // (?=.*[@#$%^&+=!*()-])  # a special character must occur at least once
+  // (?=\S+$)          # no whitespace allowed in the entire string
+  // .{8,}             # anything, at least eight places though
+  // $                 # end-of-string
+  const regPassword =
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!*()-])(?=\S+$).{8,}$/;
   if (!values.password) {
     errors.password = 'Required Password';
-  } else if (values.password.length < 6) {
-    errors.password = 'Password must be at least 6 characters';
+  } else if (values.password.length < 8) {
+    errors.password = 'Password at least 8 places though';
+  } else if (!regPassword.test(String(values.password))) {
+    errors.password = 'Invalid password';
   }
   return errors;
 }
